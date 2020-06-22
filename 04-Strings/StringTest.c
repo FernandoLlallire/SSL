229a0a06ab5c2f6f04bc1efae9bcfe1b9ad9add3
FernandoLlallire <fernandorllallire@gmail.com>
2020-06-22 04:55:26 -0300

    tp4 Terminado con makefile

--- 04-Strings/StringTest.c
+++ 04-Strings/StringTest.c
@@ -5,14 +5,18 @@
 
 int main(void){
     //Testeamos el GetLength
+    printf("Testeamos GetLength\n");
     assert(GetLength("mockString") == 10);
     //Testeamos el IsEmpty
+    printf("Testeamos IsEmpty\n");
     assert(IsEmpty("mockString") == false);
     assert(IsEmpty("") == true);
     //Testeamos el Power
+    printf("Testeamos Power\n");
     assert(strcmp(Power("mockString",2), "mockStringmockString") == 0);
     assert(strcmp(Power("mockString",2), "anotherMock") != 0);
     //Testeamos el Concat
+    printf("Testeamos Concat\n");
     assert(strcmp(Concat("mock","String"), "mockString") == 0);
     assert(strcmp(Concat("mock","String"), "anotherMock") != 0);
 }
